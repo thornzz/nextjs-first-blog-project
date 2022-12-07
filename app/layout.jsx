0bf2cefec545f5a9d740c39aad37c0a637115db8
ibrahim akgün <ibrahim@iakgun.dev>
2022-12-07 16:37:16 +0300

    First Commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,19 @@
-import './globals.css'
+import {Inter} from "@next/font/google";
+import styles from "./styles.module.css";
+import "../styles/global.css";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const interFont = Inter();
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html lang="tr" className={interFont.className}>
+      <body className={styles.container}>
+      <Header></Header>
+      <main>{children}</main>
+      <Footer></Footer>
+      </body>
     </html>
   )
 }
